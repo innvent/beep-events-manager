7c522b3a5fed0f5c76e9d18292280f7a4173d1e0
Estevão Andrade <estevaomandrade1@gmail.com>
2017-11-16 15:30:51 -0200

    criar e fechar conexao no momento que executa a query

--- lib/db_manager.rb
+++ lib/db_manager.rb
@@ -3,24 +3,31 @@ require  'singleton'
 
 class DbManager
 
-  include Singleton
-
-  def initialize    
-    @conn = PG.connect( host: get_config_value(:host), 
-                        hostaddr: get_config_value(:hostaddr), 
-                        dbname: get_config_value(:dbname), 
-                        port: get_config_value(:port),
-                        user: get_config_value(:user), 
-                        connect_timeout: get_config_value(:connect_timeout), 
-                        sslmode: get_config_value(:sslmode),
-                        password: get_config_value(:password) )
+  def self.instance
+    DbManager.new
   end
 
   def execute(sql)
-    @conn.exec(sql)
+    conn   = create_connection
+    result = conn.exec(sql)
+    conn.close
+
+    result
   end
 
   private
+
+    def create_connection
+      PG.connect( host: get_config_value(:host),
+                  hostaddr: get_config_value(:hostaddr),
+                  dbname: get_config_value(:dbname),
+                  port: get_config_value(:port),
+                  user: get_config_value(:user),
+                  connect_timeout: get_config_value(:connect_timeout),
+                  sslmode: get_config_value(:sslmode),
+                  password: get_config_value(:password) )
+    end
+
     def get_config_value(field)
       value = Beep::EventsManager::Config.instance.database_config[field]
       return (value == "") ? nil : value
