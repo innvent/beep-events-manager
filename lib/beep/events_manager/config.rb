8a36ef57c1833599942e447ff8e23c669be710f8
Luiz Costa <gutomcosta@gmail.com>
2018-04-18 18:33:23 -0300

    muda as configurações para conexão

--- lib/beep/events_manager/config.rb
+++ lib/beep/events_manager/config.rb
@@ -3,19 +3,41 @@ require  'singleton'
 module Beep
   module EventsManager
 
-  	class Config
+    class Config
 
-		 	include Singleton
+       include Singleton
 
-		  attr_accessor :object_domain, :database_config
+      attr_accessor :object_domain, :database_config
+
+      def configure_with(configuration)
+        self.database_config = configuration
+        Event.establish_connection(self.connection_config)
+      end
 
 
-		  def load_configurations(data)
-		  	@object_domain = data['object_domain']
-		  	@database_config = data['database_config']
-		  end
+      def load_configurations(data)
+        @object_domain = data['object_domain']
+        @database_config = data['database_config']
+      end
 
-  	end
+      def connection_config
+      { 
+        adapter: "postgresql",
+        encoding: "unicode",
+        database: get_config_value(:dbname),
+        host: get_config_value(:host),
+        port: get_config_value(:port),
+        username: get_config_value(:user),
+        password: get_config_value(:password)
+      }
+      end
+  
+      def get_config_value(field)
+        value =self.database_config[field]
+        return (value == "") ? nil : value
+      end
+
+    end
 
   end
 end
