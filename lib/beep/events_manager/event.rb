e7973853cd91efe51646f7ab33d58b106ab742ed
Estevão Andrade <estevaomandrade1@gmail.com>
2018-05-09 17:35:29 -0300

    retornar to_hash para evento criado

--- lib/beep/events_manager/event.rb
+++ lib/beep/events_manager/event.rb
@@ -12,7 +12,7 @@ module Beep
           object_id: self.object_id,
           object_type: self.object_type,
           object_domain: self.object_domain,
-          event_data: self.event_data,
+          event_data: self.event_data.to_hash,
           event_name: self.event_name
         }
       end
